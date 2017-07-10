f1cff8ce2a7ac77d3bd42749903ef6f7071202c3
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-07-10 15:47:23 +0200

    creates a parallel list to store prices

--- src/main/java/narrowOrParallel/implementationChange/ShoppingCart.java
+++ src/main/java/narrowOrParallel/implementationChange/ShoppingCart.java
@@ -1,7 +1,12 @@
 package narrowOrParallel.implementationChange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
     private int price;
+    
+    private List<Integer> prices = new ArrayList<>();
 
     public void add(int price) {
         this.price = price;
