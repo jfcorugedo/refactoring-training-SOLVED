1267b4e82ddc59e0ca0ae8605d4f1fb593e4647d
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-07-10 18:13:26 +0200

    adds new Price object

---
+++ src/main/java/narrowOrParallel/implementationChange/Price.java
@@ -0,0 +1,14 @@
+package narrowOrParallel.implementationChange;
+
+public class Price {
+
+	private int price;
+	
+	public Price(int price) {
+		
+	}
+	
+	public int getPrice(){
+		return this.price;
+	}
+}
