de9b197dbbe3daa7fabb139b37af4594aceca7da
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-07-12 23:32:23 +0200

    changes constructor call

--- src/main/java/narrowOrParallel/constructorFusion/Player.java
+++ src/main/java/narrowOrParallel/constructorFusion/Player.java
@@ -3,7 +3,7 @@ package narrowOrParallel.constructorFusion;
 public class Player {
 	
     public static Player createPlayer(Role role, String name, int score) {
-		return new Player(role, name, score);
+		return new Player(role, name);
 	}
 
 	private String name;
