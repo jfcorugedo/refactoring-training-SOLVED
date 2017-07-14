9b5294c67728d5507a5190f046237602e816ecd2
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-07-14 13:39:19 +0200

    calls new method from the previous one

--- src/main/java/narrowOrParallel/replaceType/SqlVehicleMatcher.java
+++ src/main/java/narrowOrParallel/replaceType/SqlVehicleMatcher.java
@@ -3,10 +3,10 @@ package narrowOrParallel.replaceType;
 public class SqlVehicleMatcher implements VehicleMatcher {
     @Override
     public int countVehicles(OldVehicleQuery query) {
-        return new VehicleRepository().find(new NewVehicleQuery(
+        return countVehicles(new NewVehicleQuery(
                 query.brandDescription.split(" ")[0],
                 query.brandDescription.split(" ")[1]
-        )).size();
+        ));
     }
 
     public int countVehicles(NewVehicleQuery query) {
